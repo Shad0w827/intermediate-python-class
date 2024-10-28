6fab9253dc96be0a7ce6b3ad9b641ecb35c768d2
Shad0w827 <paarthnath@gmail.com>
2024-10-27 21:21:40 -0400

    first commit

---
+++ revison.py
@@ -0,0 +1,43 @@
+name = "paarth"  #string
+age = 12 #integer
+score=3.88 #float
+start_learning = True #boolean
+ #data conversion
+
+#float to integer conversion
+
+score = 3.88
+print(int(score)
+      
+
+#integer to float conversion
+
+score = 4
+print(float(score))
+
+#loops
+
+#without loops
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+#with loops
+
+#for in range(10):
+#   print(i+1)
+
+count = 10
+i = 1
+sum = 0 
+while count > 0:
+sum = sum + 1
+print(sum)
+count-=1
---
+++ revison2.py
@@ -0,0 +1,36 @@
+#conditionals and conditionals
+
+#conditionals: youte given choices where you need to decide to with the one.
+
+#conditionals: the keyword that defines conditions. Like: if, else, elif, etc.
+
+number = 30
+
+if number < 30:
+    print("number less than 30")
+elif number > 30:
+    print("number greater than 30")
+    
+else:
+    print ("number is 30")
+
+print("before use of function")
+a = 9
+b = 10
+a + b = c
+print(c)
+a = 8
+b = 5
+c = a + b
+print(c)
+a = 7
+b = 9
+c = a + b
+princ(c)
+print("after use of function")
+def add(a,b):
+    c = a + b:
+    print(c)
+    add(9,10)
+    add(8,5)
+    add(7,9)
